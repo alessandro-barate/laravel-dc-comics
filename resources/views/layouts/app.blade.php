c2598b2706533ddca072d2916df2c3a1a3922d1c
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-12 17:26:49 +0200

    [UPD] Structure added to the project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <title>Laravel Template</title>
+</head>
+
+<body>
+
+    @include('shared.header')
+    
+    <main>
+        <div class="container">
+            <div class="row">
+                @yield('main')
+            </div>
+        </div>
+    </main>
+
+    @include('shared.footer')
+    
+
+    {{-- Example tag for images --}}
+    {{-- <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> --}}
+</body>
+
+</html>
