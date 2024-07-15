44155fa7d6d05d12540e53b6a8a7c80d9af65ab8
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-16 01:22:15 +0200

    [UPD] Page controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ComicController;
 use App\Http\Controllers\Guest\ComicsController;
+use App\Http\Controllers\Guest\PageController;
 use App\Models\Comic;
 use Illuminate\Support\Facades\Route;
 
@@ -16,11 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 // Homepage route
-Route::get('/', [ComicsController::class, 'index']);
+Route::get('/', [PageController::class, 'index']);
 
 Route::resource('comics', ComicController::class);
