f532f14a7891c94cf39625280ec72b02608c92c2
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-16 01:39:36 +0200

    [UPD] Foreach cycle for title visualization

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,13 +8,11 @@
 </head>
 <body>
     <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
+        @foreach ($comics as $comic)
+        <li>{{ $comic->title }}</li>    
+        @endforeach
+        
+        
     </ul>
 </body>
 </html>
